a194a2385713319be9a9f8268b9092f8b7862f5d
Dominic Grande <dominic.c.grande@gmail.com>
2018-04-13 13:26:38 -0500

    Added tests for cudnn.

--- src/cudnn/mod.rs
+++ src/cudnn/mod.rs
@@ -96,7 +96,7 @@ pub struct CudnnPoolingForwardS {
 
 add_common_fields!(
 pub struct CudnnSoftmaxForwardS {
-    pub input_vector: [u64; 7],
+    pub input_vector: [u64; 6],
     pub output_vector: [u64; 1]
 }
 );
@@ -117,39 +117,154 @@ pub enum Record {
     #[serde(rename = "cudnnSoftmaxForward")] CudnnSoftmaxForward(CudnnSoftmaxForwardS),   
 }
 
-// #[test]
-// fn cublas_cublas_create_test() {
-//     let data = r#"{"calling_tid":11358,
-//     "cublas_handle":70368511725280,
-//     "hprof_kind":"cublas","id":1,
-//     "name":"cublasCreate",
-//     "wall_end":1522732322551348279,
-//     "wall_start":1522732322054381008}"#;
-//     let v: serde_json::Value = serde_json::from_str(&data).unwrap();
-//     let r: Record = from_value(v).unwrap();
-//     match r {
-//         Record::CublasCreate(s) => assert_eq!(s.id, 1 as u64),
-//         _ => panic!("Expected a CudaSetupArgument!"),
-//     }
-// }
+#[test]
+fn cudnn_add_tensor_test() {
+    let data = r#"{"calling_tid":14585,
+    "cudnn_handle":69268213715040,
+    "hprof_kind":"cudnn",
+    "id":568964,"input_vector":[70367866280808,69268213681232,1099882827264,70367866280808,69268213665424,1099891212288],
+    "name":"cudnnAddTensor",
+    "output_vector":[1099891212288],
+    "wall_end":1522691253936525384,"wall_start":1522691253935481877}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnAddTensor(s) => assert_eq!(s.id, 568964 as u64),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
 
-// #[test]
-// fn cublas_cublas_destroy_test() {
-//      let data = r#"{"calling_tid":11358,
-//      "handle":69269201188720,
-//      "hprof_kind":"cublas",
-//      "id":8,"input_vector":[],
-//      "name":"cublasDestroy",
-//      "output_vector":[],
-//      "wall_end":1522732322554073510,
-//      "wall_start":1522732322551483898}"#;
-//     let v: serde_json::Value = serde_json::from_str(&data).unwrap();
-//     let r: Record = from_value(v).unwrap();
-//     match r {
-//         Record::CublasDestroy(s) => assert_eq!(s.handle, 69269201188720 as u64),
-//         _ => panic!("Expected a CudaSetupArgument!"),
-//     }
-// }
+#[test]
+fn cudnn_convolution_backward_bias_test() {
+    let data = r#"{"calling_tid":14585,
+    "cudnn_handle":69269000513424,
+    "hprof_kind":"cudnn","id":775,
+    "input_vector":[70367866276052,69268418360352,1099906744320,70367866276056,69268422887520],
+    "name":"cudnnConvolutionBackwardBias",
+    "output_vector":[1099882829312],
+    "wall_end":1522691204227418005,
+    "wall_start":1522691204226705392}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnConvolutionBackwardBias(s) => assert_eq!(s.input_vector, [70367866276052,69268418360352,1099906744320,70367866276056,69268422887520]),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+#[test]
+fn cudnn_convolution_backward_data_test(){
+    let data = r#"{"calling_tid":14585,
+    "cudnn_handle":69269000513424,
+    "hprof_kind":"cudnn",
+    "id":800,"input_vector":[70367866276052,69268418359456,1099889776640,69268418360352,1099906744320,69268418359968,1,1099897522176,3464,70367866276056,69268418359776,1099896225792],
+    "name":"cudnnConvolutionBackwardData",
+    "output_vector":[1099896225792],
+    "wall_end":1522691204229896916,"wall_start":1522691204228924178}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnConvolutionBackwardData(s) => assert_eq!(s.cudnn_handle, 69269000513424 as u64),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+#[test]
+fn cudnn_activation_backward_test() {
+     let data = r#"{"calling_tid":14585,
+     "cudnn_handle":69269000513424,
+     "hprof_kind":"cudnn",
+     "id":681,
+     "input_vector":[69269001368544,70367866276052,69268418360064,1099889648640,69268418360064,1099890310144,69268418360064,1099889520640,70367866276056,69268418360064],
+     "name":"cudnnActivationBackward",
+     "output_vector":[1099890182144],
+     "wall_end":1522691204220833988,
+     "wall_start":1522691204219921261}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnActivationBackward(s) => assert_eq!(s.output_vector, [1099890182144]),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+
+#[test]
+fn cudnn_activation_forward_test() {
+     let data = r#"{"calling_tid":14585,
+     "cudnn_handle":69269000513424,
+     "hprof_kind":"cudnn",
+     "id":572,"input_vector":[69269001368544,70367866280808,69268418360064,1099889520640,70367866280812,69268418360064],
+     "name":"cudnnActivationForward",
+     "output_vector":[1099889648640],"wall_end":1522691204212516355,"wall_start":1522691204211743417}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnActivationForward(s) => assert_eq!(s.output_vector, [1099889648640]),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+#[test]
+fn cudnn_convolution_backward_filter_test() {
+     let data = r#"{"calling_tid":14585,
+     "cudnn_handle":69269000513424,
+     "hprof_kind":"cudnn","id":785,
+     "input_vector":[70367866276052,69268418359776,1099894161408,69268418360352,1099906744320,69268418359968,3,1099897522176,3464,70367866276056,69268418359456,1099889876992],
+     "name":"cudnnConvolutionBackwardFilter",
+     "output_vector":[1099889876992],"wall_end":1522691204228857240,"wall_start":1522691204227480845}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnConvolutionBackwardFilter(s) => assert_eq!(s.id, 785 as u64),
+        _ => panic!("s.id test failed!")
+    }
+}
+
+#[test]
+fn cudnn_create_test() {
+     let data = r#"{"calling_tid":14585,
+     "cudnn_handle":70367866297760,
+     "hprof_kind":"cudnn","id":414070,
+     "input_vector":[],"name":"cudnnCreate","output_vector":[],
+     "wall_end":1522691239747659495,"wall_start":1522691239742154224}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnCreate(s) => assert_eq!(s.id, 414070 as u64),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+#[test]
+fn cudnn_destroy_test() {
+    let data = r#"{"calling_tid":14585,
+    "cudnn_handle":69268213715040,"hprof_kind":"cudnn",
+    "id":1654084,"input_vector":[],
+    "name":"cudnnDestroy","output_vector":[],
+    "wall_end":1522691342707328420,"wall_start":1522691342704115464}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnDestroy(s) => assert_eq!(s.id, 1654084 as u64),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
+
+#[test]
+fn cudnn_softmax_forward_test() {
+    let data = r#"{"calling_tid":14585,
+    "cudnn_handle":69269000513424,
+    "hprof_kind":"cudnn","id":82,
+    "input_vector":[69268422883968,70367866280808,69268418362016,1099891212288,70367866280812,69268418359776],
+    "name":"cudnnSoftmaxForward","output_vector":[1099894161408],"wall_end":1522691204170924921,"wall_start":1522691204169859866}"#;
+    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+    let r: Record = from_value(v).unwrap();
+    match r {
+        Record::CudnnSoftmaxForward(s) => assert_eq!(s.output_vector, [1099894161408]),
+        _ => panic!("Expected a CudaSetupArgument!"),
+    }
+}
 
 type RecordResult = Result<Record, serde_json::Error>;
 
