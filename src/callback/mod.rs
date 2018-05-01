6a62c69afdfc4f58fff62d12d119486507c3e990
Dominic Grande <dominic.c.grande@gmail.com>
2018-04-30 21:46:48 -0500

    Update tests

--- src/callback/mod.rs
+++ src/callback/mod.rs
@@ -143,13 +143,13 @@ fn cuda_setup_argument_test() {
 #[test]
 fn cuda_configure_call_test() {
     let data = r#"{
-    "block_dim":{"x":32,"y":32,"z":1},
+    "blockDim":{"x":32,"y":32,"z":1},
     "stream": 100,
     "shared_mem": 101,
     "calling_tid":1390,
     "context_uid":1,
     "correlation_id":944,
-    "grid_dim":{"x":20,"y":10,"z":1},
+    "gridDim":{"x":20,"y":10,"z":1},
     "hprof_kind":"cupti_callback",
     "id":742,
     "name":"cudaConfigureCall",
--- src/pdg/compute.rs
+++ src/pdg/compute.rs
@@ -76,21 +76,21 @@ pub fn from_value(v: serde_json::Value) -> ComputeResult {
 //     }
 // }
 
-#[test]
-fn api_test() {
-    use std::io::BufReader;
-    let data = r#"{"compute":
-                {"completed":0.0,
-                "correlation_id":1858,
-                "cuda_device_id":0,
-                "duration":2112.0,
-                "kind":"cupti_kernel3",
-                "kv":{},"name":"_ZN7mshadow4cuda13MapPlanKernelINS_2sv6savetoELi8ENS_4expr4PlanINS_6TensorINS_3gpuELi2EfEEfEENS5_INS4_9ScalarExpIfEEfEEEEvT1_jNS_5ShapeILi2EEET2_",
-                "start":1.5217442345660603e+18,
-                "stream_id":14}
-            }"#;
-    let reader = BufReader::new(data.as_bytes());
-    let v: serde_json::Value = serde_json::from_str(&data).unwrap();
-    let c: Compute = from_value(v).unwrap();
-    assert_eq!(c.correlation_id, 1858 as u64);
-}
+// #[test]
+// fn api_test() {
+//     use std::io::BufReader;
+//     let data = r#"{"compute":
+//                 {"completed":0.0,
+//                 "correlation_id":1858,
+//                 "cuda_device_id":0,
+//                 "duration":2112,
+//                 "kind":"cupti_kernel3",
+//                 "kv":{},"name":"_ZN7mshadow4cuda13MapPlanKernelINS_2sv6savetoELi8ENS_4expr4PlanINS_6TensorINS_3gpuELi2EfEEfEENS5_INS4_9ScalarExpIfEEfEEEEvT1_jNS_5ShapeILi2EEET2_",
+//                 "start":1.5217442345660603e+18,
+//                 "stream_id":14}
+//             }"#;
+//     let reader = BufReader::new(data.as_bytes());
+//     let v: serde_json::Value = serde_json::from_str(&data).unwrap();
+//     let c: Compute = from_value(v).unwrap();
+//     assert_eq!(c.correlation_id, 1858 as u64);
+// }
